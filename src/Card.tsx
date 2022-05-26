16ef12be2cbad2e73bd5550669acf857c70d1a85
Corentin Mat <corentinmat@MacBook-Pro-de-Corentin.local>
2022-05-26 15:10:02 +0200

    correction API

--- src/Card.tsx
+++ src/Card.tsx
@@ -6,17 +6,18 @@ import SVGIcon from "./img/test.svg";
 export default function MeteoCard() {
   const paris: string = "Paris";
   const berlin: string = "Berlin";
+  const newYork: string = "New-York";
+  const madrid: string = "Madrid";
+
   return (
     <div className="Container-grid">
-      <div className="container-card-sun">
-        <Data city={berlin}></Data>
-      </div>
-      <div className="container-card-sun">
-        <Data city={paris}></Data>
-      </div>
-      <div className="container-card-sun">
-        <Data city={paris}></Data>
-      </div>
+      <Data latitude="52.52" longitude="13.40" city={berlin}></Data>
+
+      <Data latitude="48.52" longitude="2.35" city={paris}></Data>
+
+      <Data latitude="40.71" longitude="-74.01" city={newYork}></Data>
+
+      <Data latitude="40.41" longitude="-3.70" city={madrid}></Data>
     </div>
   );
 }
--- src/api.tsx
+++ src/api.tsx
@@ -1,42 +1,161 @@
 import React, { useEffect, useState } from "react";
 
 import "./api.css";
+// @ts-ignore
+import soleil from "./img/iconesMeteo/soleil.png";
+// @ts-ignore
+import grossePluie from "./img/iconesMeteo/grossePluie.png";
 
-type Daily = {
-  sunset: [number];
-  winddirection_10m_dominant: [number];
-  shortwave_radiation_sum: [number];
-  precipitation_hours: [string];
-  sunrise: [string];
-  precipitation_sum: [number];
-  temperature_2m_max: [number];
-  temperature_2m_min: [number];
+type Hourly = {
+  weathercode: [number];
+  temperature_2m: [number];
   time: [string];
+  windspeed_10m: [number];
 };
+
 type Props = {
+  latitude: string;
+  longitude: string;
   city: string;
+};
+type currentWeather = {
+  time: string;
+  temperature: number;
+  weathercode: number;
+  windspeed: number;
+  winddirection: number;
 };
 
 export default function Data(props: Props) {
-  const [daily, setDaily] = useState<Daily>();
+  const [hourly, setHourly] = useState<Hourly>();
+  const [currentWeather, setCurrentWeather] = useState<currentWeather>();
 
   useEffect(() => {
     const fetchData = async function () {
       const url =
-        "https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&hourly=temperature_2m,precipitation&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,winddirection_10m_dominant,shortwave_radiation_sum&timezone=Europe%2F" +
-        props.city;
+        "https://api.open-meteo.com/v1/forecast?latitude=" +
+        props.latitude +
+        "&longitude=" +
+        props.longitude +
+        "&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true&timezone=Europe%2FBerlin";
 
       const res = await fetch(url);
       const AllData: any = await res.json();
-
-      setDaily(AllData.daily);
+      console.log(AllData);
+      setHourly(AllData.hourly);
+      setCurrentWeather(AllData.current_weather);
+      console.log(currentWeather);
     };
     fetchData();
   }, []);
 
-  if (!daily) {
-    return <div>LOADING.... </div>;
+  if (!hourly || !currentWeather) {
+    return <div>LOADING...</div>;
   } else {
-    return <div>{daily.temperature_2m_max[0]}</div>;
+    // grand soleil
+    if (currentWeather.weathercode === 0) {
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={soleil} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+
+      //soleil + nuage partiel
+    } else if (currentWeather.weathercode === 1 || 2 || 3) {
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    }
+    //brouillard +verglas
+    else if (currentWeather.weathercode === 45 || 48) {
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    } //crachin
+    else if (currentWeather.weathercode === 51 || 53 || 55 || 56 || 57)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    // pluie légère, modéré et forte
+    else if (currentWeather.weathercode === 61 || 63 || 65 || 66 || 67)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    //CHUTE DE NEIGE
+    else if (currentWeather.weathercode === 71 || 73 || 75)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    //CHUTE De grêle
+    else if (currentWeather.weathercode === 77)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    // averse légère moderé et forte
+    else if (currentWeather.weathercode === 80 || 81 || 82)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    // averse de neige légère moderé et forte
+    else if (currentWeather.weathercode === 85 || 86)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    // orage légère modéré et forte
+    else if (currentWeather.weathercode === 95 || 96 || 99)
+      return (
+        <div className="container-info-card">
+          <div className="city-text">{props.city}</div>
+          <img className="Icones-Meteo" src={grossePluie} alt="icones-meteo" />
+          <div className="data-meteo-text">{hourly.temperature_2m[0]}°C</div>
+          <div>{currentWeather.weathercode}</div>
+        </div>
+      );
+    else {
+      return <div> je sais pas quoi afficher </div>;
+    }
   }
 }
